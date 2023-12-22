62383a032b05f32119896142fc4678502dcfee2f
Fajardo-Marcial <eduardo.fajardo-marcial@hhs.gov>
2023-12-21 23:48:21 -0500

    First Commit, got the basic setup of the app running and completed

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+
+    context = {
+
+    }
+    return render(request, 'app/index.html', context)
